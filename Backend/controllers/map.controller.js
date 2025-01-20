d00d8ba5fb99864ef3fcbe61fb63daf9ede50d34
harpreet0816 <harpreet8730@gmail.com>
2025-01-21 00:37:28 +0530

    map apis creation

---
+++ Backend/controllers/map.controller.js
@@ -0,0 +1,58 @@
+const mapService = require("../services/maps.service.js");
+const {validationResut}  = require("express-validator");
+
+module.exports.getCoordinates = async (req,res,next) => {
+
+    const errors = validationResut(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { address} = req.query;
+
+    try {
+        const coordinates = await mapService.getAddressCoordinate(address);
+        res.status(200).json(coordinates);
+    } catch (error) {
+        res.status(404).json({message: "Coordinates not found"})
+    }
+
+}
+
+module.exports.getDistanceTime = async (req,res,next) => {
+
+    const errors = validationResut(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { origin , destination } = req.query;
+
+    try {
+        const distanceTime = await mapService.getDistanceTime(origin , destination);
+        res.status(200).json(distanceTime);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).json({message: "Internal server error"})
+    }
+
+}
+
+module.exports.getAutoCompleteSuggestions = async (req,res,next) => {
+
+    const errors = validationResut(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { input } = req.query;
+
+    try {
+        const suggestions = await mapService.getAutoCompleteSuggestions(input);
+        res.status(200).json(suggestions);
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).json({message: "Internal server error"})
+    }
+
+}

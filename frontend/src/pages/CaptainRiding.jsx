2cb3661a34f210d45a00cfbad2cef6a61c7170a9
harpreet0816 <harpreet8730@gmail.com>
2025-01-20 23:08:32 +0530

    captain ui complete

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -1,41 +1,74 @@
-import { Link } from "react-router-dom"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import FinishRide from "./components/FinishRide";
 
 const CaptainRiding = () => {
+
+  const [finishRidePanel, setFinishRidePanel] = useState(false);
+
+  const finishRidePanelRef = useRef(null);
+
+  useGSAP(
+    function () {
+      if (finishRidePanel) {
+        gsap.to(finishRidePanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(finishRidePanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [finishRidePanel]
+  );
+
   return (
     <div className="h-screen">
-    <div className="fixed p-6 top-0 flex items-center justify-between w-screen ">
-      <img
-        className="w-16"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
-        alt="Logo"
-      />
-      <Link
-        to="/captain-home"
-        className="h-10 w-10 bg-white flex items-center justify-center rounded-full"
+      <div className="fixed p-6 top-0 flex items-center justify-between w-screen ">
+        <img
+          className="w-16"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
+          alt="Logo"
+        />
+        <Link
+          to="/captain-home"
+          className="h-10 w-10 bg-white flex items-center justify-center rounded-full"
+        >
+          <i className="text-lg font-medium ri-logout-box-r-line"></i>
+        </Link>
+      </div>
+      <div className="h-4/5">
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt="map"
+        />
+      </div>
+      <div className="h-1/5 p-6 flex items-center justify-between bg-yellow-400 relative">
+        <h5
+          onClick={() => {
+            setFinishRidePanel(true);
+          }}
+          className="p-1 text-center w-[95%] absolute top-0"
+        >
+          <i className=" text-3xl text-black ri-arrow-up-wide-line"></i>
+        </h5>
+        <h4 className="text-lg font-semibold">4 KM away</h4>
+        <button className=" bg-green-600 text-white font-semibold p-3 px-10 rounded-lg">
+          Complete Ride
+        </button>
+      </div>
+      <div
+        ref={finishRidePanelRef}
+        className="fixed z-10 w-full bottom-0 bg-white px-3 py-6 pt-12 translate-y-full"
       >
-        <i className="text-lg font-medium ri-logout-box-r-line"></i>
-      </Link>
+        <FinishRide setFinishRidePanel={setFinishRidePanel}/>
+      </div>
     </div>
-    <div className="h-4/5">
-      <img
-        className="h-full w-full object-cover"
-        src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
-        alt="map"
-      />
-    </div>
-    <div className="h-1/5 p-6 flex items-center justify-between bg-yellow-400 relative">
-    <h5
-        onClick={() => {
-        }}
-        className="p-1 text-center w-[95%] absolute top-0"
-      >
-        <i className=" text-3xl text-black ri-arrow-up-wide-line"></i>
-      </h5>
-    <h4 className="text-lg font-semibold">4 KM away</h4>
-    <button className=" bg-green-600 text-white font-semibold p-3 px-10 rounded-lg">Complete Ride</button>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default CaptainRiding
+export default CaptainRiding;
--- frontend/src/pages/components/ConfirmRideCaptainPopup.jsx
+++ frontend/src/pages/components/ConfirmRideCaptainPopup.jsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const ConfirmRideCaptainPopup = ({setRidePopupPanel, setConfirmRidePopupPanel}) => {
+const ConfirmRideCaptainPopup = ({
+  setRidePopupPanel,
+  setConfirmRidePopupPanel,
+}) => {
+
+  const [otp, setOtp] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
   return (
     <div>
       <h5
         onClick={() => {
-            setRidePopupPanel(true);
-            setConfirmRidePopupPanel(false);
+          setRidePopupPanel(true);
+          setConfirmRidePopupPanel(false);
         }}
         className="p-1 text-center w-[93%] absolute top-0"
       >
         <i className=" text-3xl text-gray-200 ri-arrow-down-wide-line"></i>
       </h5>
-      <h3 className="text-2xl font-semibold mb-5">Confirm this ride to Start!</h3>
+      <h3 className="text-2xl font-semibold mb-5">
+        Confirm this ride to Start!
+      </h3>
       <div className="flex items-center justify-between p-3 bg-yellow-400 rounded-lg mt-4">
         <div className="flex items-center gap-3 ">
-        <img className="h-10 w-10 rounded-full object-cover" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0_hQThmfgUmHe2SM5q3kDe622hRHOBYPZlQ&s" alt="passenger poto" />
-        <h2 className="text-lg font-medium">Harsh Patel</h2>
+          <img
+            className="h-10 w-10 rounded-full object-cover"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0_hQThmfgUmHe2SM5q3kDe622hRHOBYPZlQ&s"
+            alt="passenger poto"
+          />
+          <h2 className="text-lg font-medium">Harsh Patel</h2>
         </div>
         <h5 className="text-lg font-semibold">2.2 KM</h5>
       </div>
@@ -33,7 +49,7 @@ const ConfirmRideCaptainPopup = ({setRidePopupPanel, setConfirmRidePopupPanel}) 
               </p>
             </div>
           </div>
-          <div  className="flex gap-5 p-3 border-b-2 items-center">
+          <div className="flex gap-5 p-3 border-b-2 items-center">
             <h2 className="bg-[#eee] h-8 w-12 rounded-full flex items-center justify-center">
               <i className="text-lg ri-map-pin-2-fill"></i>
             </h2>
@@ -54,18 +70,38 @@ const ConfirmRideCaptainPopup = ({setRidePopupPanel, setConfirmRidePopupPanel}) 
             </div>
           </div>
         </div>
-        <Link to="/captain-riding" className="flex justify-center w-full mt-5 bg-green-600 text-white font-semibold p-3 rounded-lg">
-          Confirm
-        </Link>
-        <button onClick={()=>{
-            setConfirmRidePopupPanel(false);
-            setRidePopupPanel(false);
-        }} className="w-full mt-1 bg-red-500 text-white font-semibold p-3 rounded-lg">
-          Cancel
-        </button>
+        <div className="mt-6 w-full">
+          <form onSubmit={submitHandler}>
+            <input
+              type="text"
+              name="otp"
+              placeholder="Enter Otp"
+              value={otp}
+              onChange={(e) => {
+                setOtp(e.target.value);
+              }}
+              className="bg-[#eee] px-6 py-4 font-mono text-lg rounded-lg w-full"
+            />
+            <Link
+              to="/captain-riding"
+              className="flex justify-center w-full mt-5 bg-green-600 text-white text-lg font-semibold p-3 rounded-lg"
+            >
+              Confirm
+            </Link>
+            <button
+              onClick={() => {
+                setConfirmRidePopupPanel(false);
+                setRidePopupPanel(false);
+              }}
+              className="w-full mt-2 bg-red-500 text-white  font-semibold p-3 rounded-lg"
+            >
+              Cancel
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ConfirmRideCaptainPopup
+export default ConfirmRideCaptainPopup;

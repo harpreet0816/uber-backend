99e882a6566dda1b6b4fdc5ce156296c4b758a22
HarpreetContriwhiz <harpeet.contriwhiz@gmail.com>
2025-01-21 16:38:37 +0530

    creation of ride apis

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -8,12 +8,12 @@ router.get('/get-coordinates',
     query("address").isString().isLength({min: 3}) ,authMiddleware.authUser, mapController.getCoordinates)
 
 router.get("/get-distance-time", 
-    query("origin"),isString().isLength({min:3}),
+    query("origin").isString().isLength({min:3}),
     query("destination").isString().isLength({min:3})
     ,authMiddleware.authUser, mapController.getDistanceTime)
 
 router.get("/get-suggestions", 
-    query(input),isString().isLength({min:3}),authMiddleware.authUser, mapController.getAutoCompleteSuggestions)
+    query("input").isString().isLength({min:3}),authMiddleware.authUser, mapController.getAutoCompleteSuggestions)
 
 module.exports = router;
 
